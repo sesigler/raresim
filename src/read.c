1608bb1c9cdf1472bb3021deb03779a171b393e8
Ryan Layer <ryan.layer@gmail.com>
2021-06-25 09:12:51 -0600

    add better command line options

--- src/read.c
+++ src/read.c
@@ -1,11 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sysexits.h>
 #include <err.h>
+#include <getopt.h>
+#include <ctype.h>
+
 #include "lists.h"
+
+int help(int exit_code)
+{
+    fprintf(stderr,
+            "usage:   read -i <input file path> -o <output file path>\n");
+    return exit_code;
+}
 
 int main(int argc, char **argv)
 {
-    struct uint32_t_sparse_matrix *m = get_matrix(argv[1]);
+
+    int c;
+    char *input_file_name = NULL;
+    char *output_file_name = NULL;
+
+    while((c = getopt (argc, argv, "i:o:")) != -1) {
+        switch(c) {
+            case 'i':
+                input_file_name = optarg;
+                break;
+            case 'o':
+                output_file_name = optarg;
+                break;
+            case 'h':
+                return help(EX_OK);
+            case '?':
+                if ( (optopt == 'i') ||
+                     (optopt == 'o') )
+                    fprintf (stderr,
+                             "Option -%c requires an argument.\n",
+                             optopt);
+                else if (isprint (optopt))
+                    fprintf (stderr,
+                             "Unknown option `-%c'.\n",
+                             optopt);
+                else
+                    fprintf(stderr,
+                            "Unknown option character `\\x%x'.\n",
+                            optopt);
+                return help(EX_USAGE);
+            default:
+                return help(EX_OK);
+        }
+    }
+
+    if (input_file_name == NULL) {
+        fprintf(stderr, "Input file required\n");
+        return help(EX_USAGE);
+    } else if (output_file_name == NULL) {
+        fprintf(stderr, "Output file required\n");
+        return help(EX_USAGE);
+    }
+
+    struct uint32_t_sparse_matrix *m = read_matrix(input_file_name);
     printf("%d\n", m->rows);
 
  
@@ -32,10 +86,5 @@ int main(int argc, char **argv)
             pruned,
             removed);
 
-    FILE *fp = fopen(argv[2], "wb");
-    if (fp == NULL)
-        err(1, "Could not open %s", argv[2]);
-
-    uint32_t ret = uint32_t_sparse_matrix_write(m, fp);
-    fclose(fp);
+    write_matrix(m, output_file_name);
 }
